dbffd9d2de15fd9fe9361d4d9ccd550fa2dffc62
Luis Ayala <lfayala9@gmail.com>
2022-10-11 21:03:24 -0400

    new things

--- persona.java
+++ persona.java
@@ -11,19 +11,6 @@ class Main {
       System.out.println("El telefono de la persona es: " + luis.getTelefono());
     }
   }
-/**
- * Para practicar la encapsulación:
-
-Crear clase Persona.
-
-Crear variables las privadas edad, nombre y telefono de la clase Persona.
-
-Crear gets y sets de cada propiedad.
-
-Crear un objeto persona en el main.
-
-Utiliza los gets y sets para darle valores a las propiedades edad, nombre y telefono, por último muéstralas por consola.
- */
 class Persona {
   private int edad;
   private String nombre;
