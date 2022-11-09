d5d1b56690dc133ae25dc5bb51fd1ca9e9bd2c0b
Luis Ayala <lfayala9@gmail.com>
2022-11-09 15:15:30 -0400

    nuevo ex

---
+++ JS/second/main.js
@@ -0,0 +1,13 @@
+const first_Object = {
+    nombre: "Luis",
+    edad: 24,
+    am_I_Dev: true,
+    birthDate: new Date(1998, 4, 16),
+
+    favBook: {
+        titulo: "Lord of the Rings",
+        autor: "JRR Tolkien",
+        fecha: 1954,
+        url: "https://www.amazon.com/Lord-Rings-J-R-R-Tolkien/dp/0544003411",
+    }
+}
