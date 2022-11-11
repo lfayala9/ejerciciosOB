de7765d2fc47c48f045a65a65649d7fc85adba3b
Luis Ayala <lfayala9@gmail.com>
2022-11-10 21:33:28 -0400

    excercise

---
+++ JS/fact/factorial-for.js
@@ -0,0 +1,5 @@
+let num = 10;
+
+for(let i = num - 1; i >= 1; i-- ){
+    console.log(num *= i)
+}
---
+++ JS/fact/factorial-while.js
@@ -0,0 +1,9 @@
+let num = 10;
+let result = num;
+
+while(num > 1){
+    num--;
+    result *= num;
+
+    console.log(result)
+}
