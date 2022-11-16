49ee6113e378c47079e7be9ede8ee16cb0207b0b
Luis Ayala <lfayala9@gmail.com>
2022-11-16 19:09:26 -0400

    new things

---
+++ JS/second/number.js
@@ -0,0 +1,7 @@
+let altura = 180
+let alturaFloat = 1.80
+let peso = 65.6
+let aluraUp = console.log(Math.ceil(alturaFloat))
+let pesoDown = console.log(Math.floor(peso))
+
+let max = console.log(Number.MAX_VALUE + 1)
