455597fb4f7eac30d133992e85daaa6235b784d1
Luis Ayala <lfayala9@gmail.com>
2022-11-16 16:29:14 -0400

    new things

---
+++ JS/second/name.js
@@ -0,0 +1,10 @@
+let nombre = "Luis"
+let apellido = "Ayala"
+let estudiante = nombre.concat(' ', apellido)
+let estudianteMayus = console.log(estudiante.toUpperCase())
+let estudianteMinus = console.log(estudiante.toLowerCase())
+let estudianteLength = console.log(estudiante.length)
+let estudianteFirst = console.log(estudiante.charAt(0))
+let estudianteLast = console.log(estudiante.substr(-1))
+let espacios = console.log(estudiante.trim())
+let estudianteTrue = console.log(estudiante.includes("Luis"))
