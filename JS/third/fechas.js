96c8c0490a41511480dc59d7fb4874fcbb9aa735
Luis Ayala <lfayala9@gmail.com>
2022-11-28 19:54:14 -0400

    new ex

---
+++ JS/third/fechas.js
@@ -0,0 +1,11 @@
+let date = new Date
+
+let birthday = new Date(1998, 4, 16)
+
+let mayorQue = date > birthday
+
+let day = birthday.getDate()
+
+let month = birthday.getMonth()
+
+let year = birthday.getFullYear()
